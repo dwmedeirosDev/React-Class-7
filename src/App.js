e4700481469f69404d367d50969d6b8d22500363
dwmedeiros94 <davidmedeiros94@hotmail.com>
2022-07-20 10:11:18 -0300

    OK

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+// import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// export default class App extends Component{
+
+//   state = {
+//     nome: "David Medeiros",
+//     idade: 28
+//   }
+
+//   mudarNome = () => {
+//     this.setState({
+//       nome: "Fernanda Flores"
+//     })
+//     document.body.style.backgroundColor = 'black'
+//     document.body.style.color = 'white'
+//   }
+
+//   render(){
+//     return(
+//       <main>
+//         <h1>Aula de React</h1>
+//         <h2>setState()</h2>
+//         <br></br>
+//         <h2>{this.state.nome}</h2>
+//         <button onClick={this.mudarNome}>Mudar nome</button>
+//       </main>
+//     )
+//   }
+// }
+
+import React, { Component } from 'react';
+
+export default class App extends Component{
+
+  state = {
+    numero: 0
+  }
+
+  somar = () => {
+    this.setState({
+      numero: this.state.numero + 1 
+    })
+  }
+
+  subtrair = () => {
+    this.setState({
+      numero: this.state.numero - 1 
+    })
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Contador</h1>
+        <h2>{this.state.numero}</h2>
+        <button onClick={this.somar}> + </button>
+        <button onClick={this.subtrair}> - </button>
+      </div>
+    )
+  }
 }
-
-export default App;
